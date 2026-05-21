b366bd0548a3d038dc9305155d8437453b503952
wandashp113 <153990895+wandashp113@users.noreply.github.com>
2026-05-22 04:11:42 +0700

    Delete v2-laravel directory

--- v2-laravel/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\MenuController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [MenuController::class, 'index'])->name('home');
-Route::resource('menu', MenuController::class)->except(['show']);
-Route::post('menu/toggle/{menu}', [MenuController::class, 'toggle'])->name('menu.toggle');
